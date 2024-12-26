1a6f9fb47e4ea2dec6ddf6a5b0fec7b99752c740
miguelpujol11 <miguelpujol905@gmail.com>
2024-12-27 00:10:31 +0100

    Actualizar

---
+++ uu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    if(InputGetKey(KeyCode.Escape)){
+        
+    }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
