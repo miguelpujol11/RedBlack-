bfca8900d5c504b4badd3642d8e24193ce57ca13
miguelpujol11 <miguelpujol905@gmail.com>
2024-12-28 23:11:56 +0100

    Actualizar uu.cs

--- uu.cs
+++ uu.cs
@@ -5,14 +5,12 @@ using UnityEngine.SceneManagement;
 public class MainPlayer : MonoBehaviour {
 
     
-    void Start () {
+void Start () {
     if(InputGetKey(KeyCode.Escape)){
       SceneManager.LoadScene(scenename);  
     }
     
-    
-    // Update is called once per frame
-    void Update () {
+  void Update () {
     
 }
     }
